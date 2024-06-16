f697e32c92b8c7d1eddc91b45f21f6dfc140f87f
Alexander Poselentsev <as.poselentsev@gmail.com>
2024-06-16 12:11:37 +0300

    add modal form, input todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+import Modal from "./components/Modal";
+
 const App = () => {
-  return (
-    <>
-      <header id="header" className="header">header</header>
-      <main id="main" className="main">main</main>
-      <footer id="footer" className="footer">footer</footer>
-    </>
-  );
+
+    const [todoList, setTodoList] = useState<any>([])
+
+    function handleSubmit(event: React.KeyboardEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log('Submit!');
+    }
+
+    return (
+        <>
+            <Modal title="Нашли ошибку?" />
+
+            <header id="header" className="header">
+                <div>header</div>
+                <div
+                    className="modal__open"
+                    // onClick={handleModal}
+                    >modal</div>
+            </header>
+
+            <main id="main" className="main">
+                <div className="todo">
+                    <form
+                        className="todo__form"
+                        onSubmit={handleSubmit}>
+                        <label className="todo__label">Что делаем?</label>
+                        <input
+                            className="todo__input"
+                            type="text"
+                            placeholder="Сделать завтрак..."
+                            />
+                    </form>
+                </div>
+            </main>
+
+            <footer id="footer" className="footer">footer</footer>
+        </>
+    );
 }
  
 export default App;

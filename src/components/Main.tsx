e221ab1a90e377e32958a9f0904bbd4c65bcc754
Alexander Poselentsev <as.poselentsev@gmail.com>
2024-07-17 13:38:28 +0300

    add require field error message

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import { UseDate } from "./hooks/getDate";
-import Task from "./Tasks/Task";
-import TaskList from "./Tasks/TaskList";
-import Empty from "./Tasks/Empty";
-import { taskItems } from "../data/todoList";
-import { Button } from "./Button";
-import { Modal } from "./Modal";
-import { ITaskItems } from "../models";
-import TaskForm from "./Tasks/TaskForm";
+import { useState } from "react"
+import { UseDate } from "./hooks/getDate"
+import Task from "./Tasks/Task"
+import TaskList from "./Tasks/TaskList"
+import Empty from "./Tasks/Empty"
+import { taskItems } from "../data/todoList"
+import { Modal } from "./Modal"
+import { ITaskItems } from "../models"
+import TaskForm from "./Tasks/TaskForm"
 
 const Main = () => {
 
@@ -15,43 +14,72 @@ const Main = () => {
     const [modalShow, setModalShow] = useState<boolean>(false)
     const modalOpen = () => setModalShow(prev => !prev)
     const modalClose = () => setModalShow(prev => !prev)
+    const [modalError, setModalerror] = useState<boolean>(false)
+    const [modalErrorText, setModalerrorText] = useState<string>('')
 
     // Работа с состоянием задач
     const [task, setTask] = useState<string>('');
     const [taskList, setTaskList] = useState<ITaskItems[]>(taskItems);
     const taskCreate = (e: React.KeyboardEvent<HTMLFormElement>) => {
-        e.preventDefault();
+        e.preventDefault()
+        if (task.trim().length === 0) {
+            setModalerror(true)
+            setModalerrorText('Поле пустое')
+            return
+        } else {
         const newTask: ITaskItems = {
             id: Date.now(),
-            title: task,
+            title: task.trim(),
             date: UseDate(),
-            status: true,
-        };
-        setTaskList(prev => [...prev, newTask]);
-        setTask('');
-        modalClose();
-    };
-    const taskCreateInput = (e: React.ChangeEvent<HTMLInputElement>) => setTask(e.target.value);
-    const taskRemove = (id: number) => setTaskList(prev => prev.filter(task => task.id !== id));
+            status: false
+        }
+        setTaskList(prev => [...prev, newTask])
+        setTask('')
+        setModalerror(false)
+        setModalerrorText('')
+        modalClose()
+        }
+    }
+    const taskCreateInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTask(e.target.value);
+        if ((task.trim().length > 0) || (e.target.value.trim().length > 0)) {setModalerror(false)}
+    }
+    const taskRemove = (id: number) => setTaskList(prev => prev.filter(task => task.id !== id))
     const taskStatusChange = (id: number, newStatus: boolean) => {
-        const target = taskList.find(t => t.id === id);
+        const target = taskList.find(t => t.id === id)
         if (target) {target.status = newStatus}
         setTaskList([...taskList])
+    }
+
+    const checkState = () => {
+        console.clear()
+        taskList.forEach((t, i) => console.log(i+1, t.status === true ? "V" : " "))
+        console.log(modalError === false ? 'Ошибки нет' : 'Ошибка есть')
     }
 
     return (
     <>
         <main id="main" className="main">
             <h2 className="title">Список задач</h2>
-            <h3 className="subtitle">Используй своё время эффективно!</h3>
-            <Button
-                title={"+ Добавить задачу"}
-                modalOpen={modalOpen}
-            />
-            <Button
-                title={"Чекнуть стейт"}
-                modalOpen={() => {taskList.forEach((t, i) => console.log(i+1, t.status === true ? "V" : " "))}}
-            />
+            <h3 className="subtitle">Используй своё время эффективно!</h3> 
+            {/* //!  Раскоментить */}
+            {/* <div className="task__create">
+                <button onClick={modalOpen}>
+                    <span>+ Добавить задачу</span>
+                </button>
+            </div>             */}
+            <div style={{display: 'flex', justifyContent:'space-between'}}>
+                <div className="task__create">
+                    <button onClick={modalOpen}>
+                        <span>+ Добавить задачу</span>
+                    </button>
+                </div>
+                <div className="task__create">
+                    <button onClick={checkState}>
+                        <span>Чекнуть стейт</span>
+                    </button>
+                </div>
+            </div>
             {taskList.length !== 0
                 ?   <TaskList>
                         {taskList.map((task, index) => <Task
@@ -70,7 +98,10 @@ const Main = () => {
             children={<TaskForm
                 value={task}
                 change={taskCreateInput}
-                create={taskCreate}/>}
+                create={taskCreate}
+                error={modalError}
+                errorText={modalErrorText}/>
+            }
         />
     </>
     );
--- src/components/Tasks/TaskForm.tsx
+++ src/components/Tasks/TaskForm.tsx
@@ -1,21 +1,26 @@
 interface TaskFormProps {
-    value: string,
-    change: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    create: (e: React.KeyboardEvent<HTMLFormElement>) => void
+    value: string;
+    change: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    create: (e: React.KeyboardEvent<HTMLFormElement>) => void;
+    error: boolean
+    errorText: string;
 }
 
-const TaskForm = ({value, change, create}: TaskFormProps) => {
+const TaskForm = ({value, change, create, error, errorText}: TaskFormProps) => {
     return (
         <form className="task__form" onSubmit={create}>
             <label htmlFor="task__input" className="task__label">Что делаем?</label>
             <input
+                style={error ? {border: 'red 4px solid'} : {}}
                 id="task__input"
                 className="task__input"
                 type="text"
                 placeholder="Сделать завтрак..."
                 value={value}
                 onChange={change}
-            />
+                />
+                {error ? <span className="task__error">{errorText}</span> : null}
+            <button type="submit">Отправить!</button>
         </form>
     );
 }
